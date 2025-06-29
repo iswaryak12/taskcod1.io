5c9b11a477ca5e9bb84f67a63fb30fdc6ee2d3a8
iswaryak12 <iswarya2006k@gmail.com>
2025-06-29 14:51:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Currently no JavaScript functionality is needed for this page
+// This file is included for future expansion if needed
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Any future JavaScript functionality can be added here
+});
